c39123ce77213eaf527599a9dcaee96748eb37b9
Branko Gagrica <bgagrica8919ri@raf.rs>
2022-02-06 12:49:40 +0100

    ff

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ app.get('/register', (req, res) => {
 
     res.sendFile('index.html', { root: './dist' });
 });
+app.get('/admin', authToken, (req, res) => {
+
+    res.sendFile('indexAdmin.html', { root: './static' });
+});
 
 
 app.post('/',authToken, (req, res) => {
